16f47140c0fd9c20e08308c52a5e9b2a83479afe
Amer rash <amerrsd@msitprogram.net>
2021-08-06 12:46:54 +0530

    1

--- movie_awards.py
+++ movie_awards.py
@@ -16,6 +16,18 @@
 #     "I, Tonya" : 1
 # }
 
-def movieAwards(oscarResults):
+def movie_awards(oscarResults):
     # Your code goes here...
-    pass
+    g={}
+    for i in oscarResults:
+        
+        if i[1] not in g:
+            
+            g[i[1]]=1
+            
+            
+        else:
+            g[i[1]]+=1
+            
+            
+    return g
